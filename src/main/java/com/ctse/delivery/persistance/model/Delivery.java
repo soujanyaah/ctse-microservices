8eb748a22f9246f67cf4f37eb40c03829006aafc
sivashankar97 <hexshankar1997@gmail.com>
2022-05-22 00:54:15 +0530

    Changes in model class

--- src/main/java/com/ctse/delivery/persistance/model/Delivery.java
+++ src/main/java/com/ctse/delivery/persistance/model/Delivery.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.util.Date;
 
 @Data
